f0e469db82f1ade4cd80fc651ff2880a214b292c
Mira Ehab <miraeha2@gmail.com>
2022-06-08 18:02:03 +0200

    Solved Problem of Day 8 (1332. Remove Palindromic Subsequences)

---
+++ 1332. Remove Palindromic Subsequences.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int start = 0, end = s.length()-1;
+        while(start <= end){
+            if(s[start] != s[end])return 2;
+            ++start;
+            --end;
+        }
+        return 1;
+    }
+};
