87d16f6826ccefac660ab43294a2adcf013f0b9e
Mira Ehab <miraeha2@gmail.com>
2022-06-14 22:29:46 +0200

    Solved Problem of Day 14 (583. Delete Operation for Two Strings)

---
+++ 583. Delete Operation for Two Strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    int smallestString(string w1, string w2) {
+        int n = w1.size(), m = w2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        
+        for (int i = 0; i <= n; i++) dp[i][0] = 0;
+        for (int i = 0; i <= m; i++) dp[0][i] = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = w1[i-1] == w2[j-1] ? 1 + dp[i-1][j-1] : max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        return dp[n][m];
+    }
+public:
+    int minDistance(string word1, string word2) {
+        int smallestS = smallestString(word1, word2);
+        return word1.size() + word2.size() - (2 * smallestS);
+    }
+};
